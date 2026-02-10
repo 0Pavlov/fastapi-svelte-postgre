67e5aca7189d2bd035b972f2294e0165e82045b1
0Pavlov <ffsvttvscsvb@gmail.com>
2026-02-10 07:05:10 +0300

    Create a database async engine and connection module

---
+++ backend/database.py
@@ -0,0 +1,38 @@
+from sqlalchemy import select, func
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine,
+    async_sessionmaker,
+    AsyncAttrs
+)
+from sqlalchemy.orm import DeclarativeBase
+
+# Connection String
+# protocol+driver://username:password@host:port/database_name
+# Use postgresql for the protocol and asyncpg driver
+# asyncpg is a very fast async driver for Python
+DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/todo_db"
+
+# Engine
+# The entry point to the database
+# echo=True prints the actual SQL queries to the console 
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Session Factory
+# Creates new database sessions for each request
+# expire_on_commit=False prevents objects from 
+# detaching after we save them, which is required 
+# for async operations
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+# Base Class 
+# All database models (tables) will inherit from this
+# It handles the mapping between Python classes and 
+# SQL tables
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
