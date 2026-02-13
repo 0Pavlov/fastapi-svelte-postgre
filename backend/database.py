557bd13066573f925a9810566bb165aa8b8b5289
0Pavlov <ffsvttvscsvb@gmail.com>
2026-02-13 08:49:33 +0300

    Change the db url for it to work with docker

--- backend/database.py
+++ backend/database.py
@@ -11,7 +11,10 @@ from sqlalchemy.orm import DeclarativeBase
 # protocol+driver://username:password@host:port/database_name
 # Use postgresql for the protocol and asyncpg driver
 # asyncpg is a very fast async driver for Python
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/todo_db"
+# Use this url if running all manually
+#DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/todo_db"
+# Use this url if running with docker
+DATABASE_URL = "postgresql+asyncpg://postgres:postgres@db:5432/todo_db"
 
 # Engine
 # The entry point to the database
