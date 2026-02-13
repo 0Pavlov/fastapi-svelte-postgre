e3fb400eebc2b5a5671fd39edc9b54271d044f0e
0Pavlov <ffsvttvscsvb@gmail.com>
2026-02-13 08:47:07 +0300

    Add a proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,4 +2,13 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({
+  plugins: [tailwindcss(), sveltekit()],
+  server: {
+    host: true, // needed for Docker '--host'
+    port: 5173,
+    proxy: {
+      '/api': 'http://backend:8000' // Proxy (use just /api to call / backend endpoint)
+    }
+  }
+});
